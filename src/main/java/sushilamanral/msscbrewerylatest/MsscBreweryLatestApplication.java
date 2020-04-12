c5a5da399ae0adaa192ff6e048ee05e4c328fe88
sushila <sushila.manral@gmail.com>
2020-04-12 15:39:42 +0530

    Initial commit to create project

---
+++ src/main/java/sushilamanral/msscbrewerylatest/MsscBreweryLatestApplication.java
@@ -0,0 +1,13 @@
+package sushilamanral.msscbrewerylatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryLatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryLatestApplication.class, args);
+	}
+
+}
